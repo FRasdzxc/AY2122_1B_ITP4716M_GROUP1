7386ffabcee23006c6aa635fdac1a5eef95e7524
Spirits0u0 <paulwen03828@gmail.com>
2022-07-14 03:24:35 +0800

    add

--- ITP4715_Group1_AmazingCurling/Assets/Scripts/PassNumber.cs
+++ ITP4715_Group1_AmazingCurling/Assets/Scripts/PassNumber.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PassNumber : MonoBehaviour
 {
+
+    public float ran;
     private void Start()
     {
         PlayerPrefs.SetInt("venue", 0);
     }
-    public float ran;
+    
     public void passRound(int num)
     {
         PlayerPrefs.SetInt("round", num);
@@ -18,4 +21,5 @@ public class PassNumber : MonoBehaviour
         ran = Random.Range(1, 10);
         PlayerPrefs.SetFloat("round", ran);
     }
+    
 }
